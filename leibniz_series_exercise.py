5fb2ad19dea10ed265debbf4c7c5923c9b27056c
319129714 <roishvimer@gmail.com>
2025-11-12 13:51:12 +0200

    Update leibniz_series_exercise.py

--- leibniz_series_exercise.py
+++ leibniz_series_exercise.py
@@ -1,2 +1,10 @@
 def approximate_pi(n_terms):
-    pass # replace pass with your code
+    leibniz_sum = 0.0
+    for i in range(n_terms):
+        sign = (-1)**i
+        denominator = 2 * i + 1
+        term = sign / denominator
+        leibniz_sum += term
+    pi_approximation = 4 * leibniz_sum
+    
+    return pi_approximation
